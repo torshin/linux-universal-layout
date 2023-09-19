9f78e700bde602c2b7d40f6fb8ac1836a91bda5e
torshin <mittyta@ya.ru>
2023-09-19 12:06:12 +0300

    Update .on-new-kb.sh

--- linux/cinnamon-home-folder/.on-new-kb.sh
+++ linux/cinnamon-home-folder/.on-new-kb.sh
@@ -1,12 +1,11 @@
 #! /bin/sh
-keymap=~/.config/xkb
 
 echo >&2 "$@"
 event=$1 id=$2 type=$3
 
-case "$event $type" in
-'XIDeviceEnabled XISlaveKeyboard')
+case "$type" in
+'XISlaveKeyboard')
         xset r rate 200 50
-        setxkbmap -layout "uni_fork(us_universal),uni_fork(ru_universal)" -option "" -option "grp_led:scroll,lv3:rwin_switch,grp:win_space_toggle" -print | xkbcomp -I"$HOME/.config/xkb" - "${DISPLAY%%.*}" -w 1
+        sh ~/.xkb.sh
         echo "success"
 esac
